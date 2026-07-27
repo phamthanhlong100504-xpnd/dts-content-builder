5464798ddc7680b6c697bb71d37b05c25b275384
phamthanhlong1005 <166156509+phamthanhlong100504-xpnd@users.noreply.github.com>
2026-07-27 11:20:25 +0700

    basic

---
+++ src/main/java/com/dts/content_builder/ContentBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.dts.content_builder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentBuilderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dts/content_builder/ContentBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dts.content_builder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
