b98d9d5857842fcc9cf51e5a99ade47fa3226841
phamthanhlong1005 <166156509+phamthanhlong100504-xpnd@users.noreply.github.com>
2026-08-02 23:56:38 +0700

    update connect synchorius

--- src/main/java/com/dts/content_builder/ContentBuilderApplication.java
+++ src/main/java/com/dts/content_builder/ContentBuilderApplication.java
@@ -3,7 +3,12 @@ package com.dts.content_builder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class ContentBuilderApplication {
 
 	public static void main(String[] args) {
